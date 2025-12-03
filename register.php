32b33db51df3efe682a649caea7f0eaf2c983ab9
nyameroneko50-beep <nyameroneko50@gmail.com>
2025-12-03 08:24:58 +0800

    Update register.php

--- register.php
+++ register.php
@@ -5,15 +5,12 @@ $user = "if0_40563746";
 $pass = "YOUR_VPANEL_PASSWORD_HERE"; 
 $dbname = "if0_40563746_registration_db";
 
-// Connect to database
 $conn = new mysqli($host, $user, $pass, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection Failed: " . $conn->connect_error);
 }
 
-// Get form data
 $username = $_POST['username'];
 $fullname = $_POST['fullname'];
 $email = $_POST['email'];
@@ -21,7 +18,6 @@ $phone = $_POST['phone'];
 $gender = $_POST['gender'];
 $password = $_POST['password'];
 
-// Insert into DB
 $sql = "INSERT INTO users (username, fullname, email, phone, gender, password)
         VALUES ('$username', '$fullname', '$email', '$phone', '$gender', '$password')";
 
