6d653da7102e5f06fe79a596c5968e696d40dde5
nyameroneko50-beep <nyameroneko50@gmail.com>
2025-12-03 21:42:03 +0800

    Update delete_user.php

--- delete_user.php
+++ delete_user.php
@@ -7,17 +7,14 @@ if(isset($_GET['id'])){
     $sql = "DELETE FROM users WHERE id=$id";
 
     if($conn->query($sql) === TRUE){
-
         echo "
         <h2>User deleted successfully!</h2>
-        <a href='view_users.php'>Back to View Users</a><br><br>
-        <a href='index.html'>Back to Registration Form</a>
+        <a href='view_users.php'><button>Back to View Users</button></a>
+        <a href='index.html'><button>Back to Registration Form</button></a>
         ";
-
     } else {
         echo 'Error deleting record: ' . $conn->error;
     }
-
 } else {
     echo 'No ID provided!';
 }
