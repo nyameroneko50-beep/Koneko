97d5fb03d630c2f4c20a47aa1b83d720d89b6f1e
nyameroneko50-beep <nyameroneko50@gmail.com>
2025-12-03 13:46:46 +0800

    Create db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,12 @@
+<?php
+$host = "sql110.infinityfree.com"; 
+$user = "if0_40563746";
+$pass = "YOUR_VPANEL_PASSWORD"; 
+$db   = "if0_40563746_registration_db";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
