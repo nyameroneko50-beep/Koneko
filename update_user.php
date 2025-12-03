24fc4c132c7bb13da4cd99b70312e9f52ba2d444
nyameroneko50-beep <nyameroneko50@gmail.com>
2025-12-03 21:38:58 +0800

    Update update_user.php

--- update_user.php
+++ update_user.php
@@ -5,35 +5,41 @@ $pass = "mOyAlhhHuQ";
 $dbname = "if0_40563746_registration_db";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
-
 if ($conn->connect_error) {
     die("Connection Failed: " . $conn->connect_error);
 }
 
-$id = $_POST['id'];
-$username = $_POST['username'];
-$fullname = $_POST['fullname'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$gender = $_POST['gender'];
+if(isset($_POST['update'])){
 
-$sql = "UPDATE users SET
-        username='$username',
-        fullname='$fullname',
-        email='$email',
-        phone='$phone',
-        gender='$gender'
-        WHERE id=$id";
+    $id = $_POST['id'];
+    $username = $_POST['username'];
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $gender = $_POST['gender'];
 
-if ($conn->query($sql) === TRUE) {
+    $sql = "UPDATE users SET
+            username='$username',
+            fullname='$fullname',
+            email='$email',
+            phone='$phone',
+            gender='$gender'
+            WHERE id=$id";
 
-    echo "
-    <h2>User updated successfully!</h2>
-    <a href='view_users.php'>Back to View Users</a><br><br>
-    <a href='index.html'>Back to Registration Form</a>
-    ";
+    if ($conn->query($sql) === TRUE) {
+        echo "
+        <h2>User updated successfully!</h2>
+        <a href='view_users.php'><button>Back to View Users</button></a>
+        <a href='index.html'><button>Back to Registration Form</button></a>
+        ";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
 
 } else {
-    echo 'Error: ' . $conn->error;
+    echo "<h2>No data received! Please submit the form.</h2>";
+    echo "<a href='index.html'><button>Back to Registration Form</button></a>";
 }
+
+$conn->close();
 ?>
